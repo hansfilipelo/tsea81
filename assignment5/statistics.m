a88643fc924ebb0f6f989188f75210bee5eb32e7
Frida Sundberg <sundberg.fr@gmail.com>
2015-12-17 16:19:24 +0100

    Added statistics for each passenger for modified ass3/ass4

--- assignment5/statistics.m
+++ assignment5/statistics.m
@@ -1,1 +1,45 @@
-statFolder = "ass3";statFolder2 = "mod_ass3";passengers = [10, 20, 30, 40, 50, 60, 70];stdDev = [];myMean = [];stdDev2 = [];myMean2 = [];for i = 1:length(passengers)    A = load([statFolder "/stat_" num2str(passengers(i)) ".txt"]);  B = load([statFolder2 "/stat_" num2str(passengers(i)) ".txt"]);    disp(["Standard deviation " num2str(passengers(i))]);  tmpDev = sqrt(var(A))  stdDev = [stdDev tmpDev];  % Optimized version  tmpDev2 = sqrt(var(B))  stdDev2 = [stdDev2 tmpDev2];    disp(["Mean " num2str(passengers(i))]);  tmpMean = mean(A)  myMean = [myMean tmpMean];  % Optimized version  tmpMean2 = mean(B)  myMean2 = [myMean2 tmpMean2];  disp("");endfigure 1plot(passengers,stdDev,'b',passengers,stdDev2,'r')legend("Un-optimized version","Optimized version");title("Standard deviations");xlabel("Passengers (nr)");ylabel("StdDev");figure 2plot(passengers,myMean,'b',passengers,myMean2,'r')legend("Un-optimized version","Optimized version");title("Average lift travel times");xlabel("Passengers (nr)");ylabel("Avg travel times");
+statFolder = 'ass3';
+statFolder2 = 'mod_ass3';
+passengers = [10, 20, 30, 40, 50, 60, 70];
+stdDev = [];
+myMean = [];
+stdDev2 = [];
+myMean2 = [];
+
+for i = 1:length(passengers)
+  
+  A = load([statFolder '/stat_' num2str(passengers(i)) '.txt']);
+  B = load([statFolder2 '/stat_' num2str(passengers(i)) '.txt']);
+  
+  %disp(['Standard deviation ' num2str(passengers(i))]);
+  tmpDev = sqrt(var(A));
+  stdDev = [stdDev tmpDev];
+  % Optimized version
+  tmpDev2 = sqrt(var(B));
+  stdDev2 = [stdDev2 tmpDev2];
+  
+  %disp(['Mean ' num2str(passengers(i))]);
+  tmpMean = mean(A);
+  myMean = [myMean tmpMean];
+  % Optimized version
+  tmpMean2 = mean(B);
+  myMean2 = [myMean2 tmpMean2];
+ 
+  %disp('');
+end
+
+figure(1)
+plot(passengers,stdDev,'b',passengers,stdDev2,'r')
+legend('Un-optimized version','Optimized version');
+title('Standard deviations Ass3');
+xlabel('Number of passengers');
+ylabel('StdDev [usec]');
+
+figure(2)
+plot(passengers,myMean,'b',passengers,myMean2,'r')
+legend('Un-optimized version','Optimized version');
+title('Average lift travel times Ass3');
+xlabel('Number of passengers');
+ylabel('Avg travel times [usec]');
+
+
--- assignment5/statistics_ass4.m
+++ assignment5/statistics_ass4.m
@@ -1,6 +1,6 @@
-statFolder = "ass4";
-statFolder2 = "queue_ass4";
-statFolder3 = "mod_ass4";
+statFolder = 'ass4';
+statFolder2 = 'queue_ass4';
+statFolder3 = 'mod_ass4';
 passengers = [10, 20, 30, 40, 50, 60, 70];
 stdDev = [];
 myMean = [];
@@ -11,42 +11,42 @@ myMean3 = [];
 
 for i = 1:length(passengers)
   
-  A = load([statFolder "/stat_" num2str(passengers(i)) ".txt"]);
-  B = load([statFolder2 "/stat_" num2str(passengers(i)) ".txt"]);
-  C = load([statFolder3 "/stat_" num2str(passengers(i)) ".txt"]);
+  A = load([statFolder '/stat_' num2str(passengers(i)) '.txt']);
+  B = load([statFolder2 '/stat_' num2str(passengers(i)) '.txt']);
+  C = load([statFolder3 '/stat_' num2str(passengers(i)) '.txt']);
   
-  disp(["Standard deviation " num2str(passengers(i))]);
-  tmpDev = sqrt(var(A))
+  %disp(['Standard deviation ' num2str(passengers(i))]);
+  tmpDev = sqrt(var(A));
   stdDev = [stdDev tmpDev];
   % Queue version
-  tmpDev2 = sqrt(var(B))
+  tmpDev2 = sqrt(var(B));
   stdDev2 = [stdDev2 tmpDev2];
   % Optimized version
-  tmpDev3 = sqrt(var(C))
+  tmpDev3 = sqrt(var(C));
   stdDev3 = [stdDev3 tmpDev3];
   
-  disp(["Mean " num2str(passengers(i))]);
-  tmpMean = mean(A)
+  %disp(['Mean ' num2str(passengers(i))]);
+  tmpMean = mean(A);
   myMean = [myMean tmpMean];
   % Queue version
-  tmpMean2 = mean(B)
+  tmpMean2 = mean(B);
   myMean2 = [myMean2 tmpMean2];
   % Optimized version
-  tmpMean3 = mean(C)
+  tmpMean3 = mean(C);
   myMean3 = [myMean3 tmpMean3];
-  disp("");
+  %disp('');
 end
 
-figure 1
+figure(1)
 plot(passengers,stdDev,'b',passengers,stdDev2,'r',passengers,stdDev3,'k')
-legend("Un-optimized version","Queue version","Optimized version");
-title("Standard deviations");
-xlabel("Passengers (nr)");
-ylabel("StdDev");
+legend('Un-optimized version','Queue version','Optimized version');
+title('Standard deviations Ass4');
+xlabel('Number of passengers');
+ylabel('StdDev [usec]');
 
-figure 2
+figure(2)
 plot(passengers,myMean,'b',passengers,myMean2,'r',passengers,myMean3,'k')
-legend("Un-optimized version","Queue version","Optimized version");
-title("Average lift travel times");
-xlabel("Passengers (nr)");
-ylabel("Avg travel times");
+legend('Un-optimized version','Queue version','Optimized version');
+title('Average lift travel times Ass4');
+xlabel('Number of passengers');
+ylabel('Avg travel times [usec]');
